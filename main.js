c64db9a6f35c9b44a04bb8076a67dbb3cc2f7148
Yanca Fernandes <yancafernandes2@gmail.com>
2023-03-20 10:52:26 -0500

    Finalizando o projeto fazendo condicional com while

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//listaDeTeclas.length garante pegar a quantidade total de teclas da lista ainda que não saiba quantas são
+//length serve pra pegar a quantidade (significa tamanho)
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    
+    //Template String
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio);
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    //console.log(contador);
+}
